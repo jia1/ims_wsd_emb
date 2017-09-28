81e7105607cd2f81e2bf461195347d301a956eb1
Jia Yee <jiayeerawr@gmail.com>
2017-09-29 01:44:05 +0800

    Add processing script for lexical sample task
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

---
+++ proc.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+args = sys.argv
+
+if len(args) len(args) != 2:
+    exit('Usage: python proc.py result_file/directory')
+
+result_file = args[1]
+
+def process_one(result_file):
+    process_one(result_file):
+        processed_lines = []
+    with open(result_file, 'r') as f:
+        for line in f:
+            split_line = line.split(' ')
+            if len(split_line) > 1:
+                split_line[0] = split_line[1].split('.')[0]
+            processed_lines.append(' '.join(split_line))
+    with open(result_file, 'w') as g:
+        g.writelines(processed_lines)
+
+if os.path.isdir(result_file):
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    for f in files:
+        process_one(f)
+else:
+    process_one(result_file)
