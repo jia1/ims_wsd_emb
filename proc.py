6921e22fbf3dc4684daee7358eb57f2034b25265
Jia Yee <jiayeerawr@gmail.com>
2017-10-10 21:59:08 +0800

    Process files with missing ID
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- proc.py
+++ proc.py
@@ -12,10 +12,15 @@ def process_one(result_file):
     processed_lines = []
     with open(result_file, 'r') as f:
         for line in f:
-            split_line = line.split(' ')
-            if len(split_line) > 1:
-                split_line[0] = split_line[1].split('.')[0]
-            processed_lines.append(' '.join(split_line))
+            split_line = line.strip().split(' ')
+            if len(split_line) >= 3:
+                word_id = split_line[1].split('.')[0].split('%')[0]
+                best_sense, best_prob = '', 0
+                for i in range(2, len(split_line)-1, 2):
+                    curr_sense, curr_prob = split_line[i], split_line[i+1]
+                    if curr_prob > best_prob:
+                        best_sense, best_prob = curr_sense, curr_prob
+                processed_lines.append('{0} {1} {2}\n'.format(word_id, split_line[1], best_sense))
     with open(result_file, 'w') as g:
         g.writelines(processed_lines)
 
