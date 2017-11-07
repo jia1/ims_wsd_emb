c8992f1abe4a290ef125bf15aa5fe617f17d392b
Jia Yee <jiayeerawr@gmail.com>
2017-11-07 23:56:20 +0800

    Add mkdir and $1 to train-omsti
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- train-omsti.sh
+++ train-omsti.sh
@@ -1,12 +1,13 @@
-POS=adv
+POS=${1}
 
 TAGGED_CORPUS=omsti
 DATA_DIR=../${TAGGED_CORPUS}-data
 
-# EMB_OPT_SRC=w2v-800-win-10-wikiFeb2017
-EMB_OPT_SRC=w2v-100-win-10-1b
+EMB_OPT_SRC=w2v-800-win-10-wikiFeb2017
 
 MODEL_DIR=../models-${TAGGED_CORPUS}-${EMB_OPT_SRC}
+
+mkdir -p $MODEL_DIR
 
 nohup time ./train_one.bash $DATA_DIR/$POS $DATA_DIR/$POS \
     $MODEL_DIR &> nohup-train-${TAGGED_CORPUS}-${EMB_OPT_SRC}-${POS}.out &
